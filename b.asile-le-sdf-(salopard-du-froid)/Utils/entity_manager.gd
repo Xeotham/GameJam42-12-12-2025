b4e49368fc7d89eabc7aa1c48965cd74585669b7
mhaouas <mhaouas@student.42angouleme.fr>
2025-12-14 11:33:46 +0100

    test

--- b.asile-le-sdf-(salopard-du-froid)/Utils/entity_manager.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-#signal spawn_collectible(type: Collectible.Type, initial_state: Collectible.States, collectible_global_position: Vector2, collectible_direction: Vector2)
-signal spawn_enemy(enemy_data: EnemyData)
-signal death_enemy(enemy: Character)
